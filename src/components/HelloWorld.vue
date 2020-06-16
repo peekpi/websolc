b5177a4ae11147eeb6a41120cc7801806a2121d1
lixp <894646171@qq.com>
2020-06-16 18:12:05 +0800

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <h1>{{ msg }}</h1>
+        <button @click="ok">hello</button>
+        <div id="container" style="height:500px;"></div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+//import "browser-solc"
+import * as monaco from 'monaco-editor'
+const solcjs = require("solc-js");
+//const ResolverEngine = require('solc-resolver').resolverEngine;
+const sourceCode = `
+pragma solidity >0.4.99 <0.6.0;
+
+library OldLibrary {
+  function someFunction(uint8 a) public returns(bool);
 }
+
+contract NewContract {
+  function f(uint8 a) public returns (bool) {
+      return OldLibrary.someFunction(a);
+  }
+}`;
+
+let editor;
+function monacoInit(){
+  window.monaco = monaco;
+    editor = monaco.editor.create(document.getElementById("container"), {
+    value: sourceCode,
+    language: "Solidity",
+
+    lineNumbers: "on",
+    roundedSelection: false,
+    scrollBeyondLastLine: true,
+    readOnly: false,
+    theme: "vs-dark",
+  });
+  window.editor = editor;
+}
+let compiler;
+export default {
+    name: "HelloWorld",
+    data(){
+      console.log("data");
+      return {};
+    },
+    props: {
+        msg: String
+    },
+    async mounted(){
+      const version = "v0.5.1-stable-2018.12.03";
+      compiler = await solcjs(version);
+      monacoInit();
+    },
+    methods: {
+        async ok() {
+
+            //console.log(solcjs);
+            //console.log(ResolverEngine);
+
+            // or
+
+            // const compiler = await solcjs()
+
+            const output = await compiler(editor.getValue(), e=>e?"":"");
+
+            //var wrapper = require('solc-wrapper');
+            //var solc = wrapper(window.Module);
+            //console.log(wrapper);
+            console.log(output);
+        }
+    }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
