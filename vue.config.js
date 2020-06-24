b3188fe89736d2fd9b040e0b0ef40577cf1bd7f7
lixp <894646171@qq.com>
2020-06-24 21:27:17 +0800

    fixed demo

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   publicPath: isProduction
-    ? '/solc/dist/'
+    ? '/websolc/dist/'
     : '/',
   productionSourceMap: isProduction
     ? false : true,
 }
+/*
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+module.exports = {
+    configureWebpack: {
+        plugins: [new MonacoWebpackPlugin(['Solidity'])]  
+    },
+}*/
